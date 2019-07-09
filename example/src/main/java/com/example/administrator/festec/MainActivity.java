ccca7037aff630131c1130edf200561122a989f7
sikeziji <1931696152@qq.com>
2019-07-09 17:46:21 +0800

    添加AVloading代码

--- example/src/main/java/com/example/administrator/festec/MainActivity.java
+++ example/src/main/java/com/example/administrator/festec/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.administrator.festec;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.latte_core.app.Latte;
-import com.example.latte_core.ui.loader.LatteLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LatteLoader.showLoading(this);
     }
 }
